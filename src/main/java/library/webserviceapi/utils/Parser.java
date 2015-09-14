7a02407d79c2c786070e96b44a68caff8f347457
Borja Velasco <borja.velasco@babel.es>
2015-09-14 13:50:30 +0200

    Añadida clase parser para pasar de json a objeto y de objeto a json

--- src/main/java/library/webserviceapi/utils/Parser.java
+++ src/main/java/library/webserviceapi/utils/Parser.java
@@ -1,18 +1,31 @@
 package library.webserviceapi.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.IOException;
 
 /**
  * Created by BABEL Sistemas de Información.
  */
-public abstract class BaseRequest {
+public class Parser {
 
     public static String getJson(Object aClass) throws JsonProcessingException {
         ObjectWriter sObjectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
         return sObjectWriter.writeValueAsString(aClass);
     }
 
+    public static Object parseResult(String json, Class<?> aClass) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        try {
+            return objectMapper.readValue(json, aClass);
+        } catch (IOException e) {
+        }
+        return null;
+    }
 
 }
